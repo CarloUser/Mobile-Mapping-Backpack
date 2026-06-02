82146c21334f7a0ad0797d27ac8d2ad90b265918
CarloUser <carlobronold@gmail.com>
2026-06-02 12:44:04 +0200

    Add files via upload

---
+++ calibrate_oaklite_back.py
@@ -0,0 +1,13 @@
+import intrinsic_calibration_core as core
+
+
+def main():
+    args = core.parse_args()
+    if args.list_devices:
+        core.print_devices(args)
+        return
+    core.run_camera("oaklite_back", args)
+
+
+if __name__ == "__main__":
+    main()
